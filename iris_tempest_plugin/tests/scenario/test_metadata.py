efc0b65c5f9d56ca12e0fb24490ba86c98b36cc7
Will Szumski <will@stackhpc.com>
2019-02-27 17:30:52 +0000

    Initial PoC for the apel test

--- iris_tempest_plugin/tests/scenario/test_metadata.py
+++ iris_tempest_plugin/tests/scenario/test_metadata.py
@@ -15,14 +15,16 @@
 
 from collections import namedtuple
 
+from tempest.api import compute
 from tempest import config
+from tempest.lib.common import api_version_utils
 from tempest.lib import decorators
 from tempest.lib import exceptions as lib_exc
+from tempest.lib.exceptions import TempestException
 
 from iris_tempest_plugin.tests import base
 
 CONF = config.CONF
-LIMIT = None
 
 ImageSearchResult = namedtuple("SearchResult", "success image")
 
@@ -33,6 +35,7 @@ def _params(**kwargs):
 
 
 class GlanceMetaDataTest(base.TestCase):
+    LIMIT = None
 
     @staticmethod
     def _contains(images, meta):
@@ -57,7 +60,7 @@ class GlanceMetaDataTest(base.TestCase):
             raise lib_exc.InvalidConfiguration(
                 'Either api_v1 or api_v2 must be True in '
                 '[image-feature-enabled].')
-        response = glance_client.list_images(params=_params(limit=LIMIT))
+        response = glance_client.list_images(params=_params(limit=self.LIMIT))
         # TODO(wszumski): check response http code and possibly retry
         while True:
             images = response["images"]
@@ -71,7 +74,118 @@ class GlanceMetaDataTest(base.TestCase):
                 break
             last = images[-1]
             response = glance_client.list_images(params=_params(
-                limit=LIMIT, marker=last["id"])
+                limit=self.LIMIT, marker=last["id"])
             )
 
         self.assertTrue(result.success)
+
+
+class SpecMissing(TempestException):
+    message = "The spec %(spec)s is missing on the flavor with uuid %(uuid)s"
+
+
+class NoExtraSpecs(TempestException):
+    message = (
+        "The flavor with uuid %(uuid)s does not have any extra specs defined"
+    )
+
+
+class SpecInvalid(TempestException):
+    def __init__(self, msg, *args, **kwargs):
+        self.message = msg
+        super(SpecInvalid, self).__init__(*args, **kwargs)
+
+
+class ComputeTest(base.TestCase):
+
+    def setUp(self):
+        super(ComputeTest, self).setUp()
+        if hasattr(self, "request_microversion"):
+            self.useFixture(
+                compute.api_microversion_fixture.APIMicroversionFixture(
+                    self.request_microversion))
+
+    @classmethod
+    def skip_checks(cls):
+        super(ComputeTest, cls).skip_checks()
+        if not CONF.service_available.nova:
+            raise cls.skipException("Nova is not available")
+        cfg_min_version = CONF.compute.min_microversion
+        cfg_max_version = CONF.compute.max_microversion or 'latest'
+        if hasattr(cls, "min_microversion") and hasattr(cls,
+                                                        "max_microversion"):
+            api_version_utils.check_skip_with_microversion(
+                cls.min_microversion,
+                cls.max_microversion,
+                cfg_min_version,
+                cfg_max_version)
+
+    @classmethod
+    def resource_setup(cls):
+        super(ComputeTest, cls).resource_setup()
+        if hasattr(cls, "min_microversion"):
+            cls.request_microversion = (
+                api_version_utils.select_request_microversion(
+                    cls.min_microversion,
+                    CONF.compute.min_microversion))
+
+
+class NovaFlavorTest(ComputeTest):
+    LIMIT = None
+
+    # NOTE(wszumski): in 2.61 we can get the extra specs from a list_detailed
+    # call: https://developer.openstack.org/api-ref/compute/?expanded=list-flavors-with-details-detail#list-flavors-with-details  # noqa E501
+    # min_microversion = '2.61'
+    # max_microversion = 'latest'
+
+    def _foreach_check_extra_specs(self, flavors, specs):
+        client = self.client
+        for flavor in flavors:
+            response = client.list_flavor_extra_specs(flavor["id"])
+            extra_specs = response["extra_specs"]
+            for spec, action in specs.items():
+                validator = action["validator"]
+                if spec not in extra_specs:
+                    raise SpecMissing(spec=spec, uuid=flavor["id"])
+                value = extra_specs[spec]
+                if not validator(value):
+                    failure_tmpl = (
+                        "%(spec)s on the flavor with uuid "
+                        "%(flavor_id)s is set to %(value)s. %(msg)s"
+                    )
+                    fail_msg = failure_tmpl % {
+                        "msg": action["fail_msg"],
+                        "spec": spec,
+                        "flavor_id": flavor["id"],
+                        "value": value
+                    }
+                    raise SpecInvalid(fail_msg)
+
+    def setUp(self):
+        super(NovaFlavorTest, self).setUp()
+        self.client = self.os_primary.flavors_client
+
+    def test_apel_cpu_cost_on_every_flavor(self):
+        client = self.client
+        extra_specs = {
+            "APEL_CPU_COST": {
+                "validator": unicode.isdigit,
+                "fail_msg": "Was expecting an int."
+            }
+        }
+        response = client.list_flavors(detail=True,
+                                       **_params(limit=self.LIMIT))
+        while True:
+            flavors = response["flavors"]
+            self._foreach_check_extra_specs(flavors, extra_specs)
+            flavor_links = response["flavors_links"]
+            has_next = bool([x for x in flavor_links if x["rel"] == "next"])
+            if not has_next:
+                break
+            next_ = flavors[-1]
+            response = client.list_flavors(
+                detail=True, **_params(
+                    limit=self.LIMIT,
+                    marker=next_["id"]
+                )
+            )
